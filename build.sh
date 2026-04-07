c12483b0cf721a6f245b5182b1a26cfebd0aa17f
Ceyjie <ceyjie@github.com>
2026-04-07 23:52:35 +0800

    Update build.sh with simplified compilation flags

--- build.sh
+++ build.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
-# For Orange Pi One (ARM Cortex-A7) cross-compilation
-# Install toolchain: sudo apt install gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
-# Then compile with: arm-linux-gnueabihf-g++ ...
-
-# Check if cross-compiling
-if [ -n "$CROSS_COMPILE" ]; then
-    # Cross-compilation for Orange Pi One
-    CFLAGS="-O3 -march=armv7-a -mfloat-abi=hard -mfpu=neon-vfpv4 -mtune=cortex-a7 -std=c++17"
-    arm-linux-gnueabihf-g++ $CFLAGS -o pipaint drawingcanvas.cpp touchhandler.cpp main.cpp \
-        -I/usr/include/arm-linux-gnueabihf/SDL2 -lSDL2_image -lSDL2_ttf -levdev
-else
-    # Host compilation (x86_64/aarch64)
-    CFLAGS="-O3 -flto -std=c++17 -I/usr/include/libevdev-1.0"
-    LDFLAGS="-flto"
-
-    g++ $CFLAGS -o pipaint drawingcanvas.cpp touchhandler.cpp main.cpp \
-        `sdl2-config --cflags --libs` -lSDL2_image -lSDL2_ttf -levdev $LDFLAGS
-fi
+g++ -std=c++17 -O2 -I/usr/include/libevdev-1.0 -o pipaint \
+    drawingcanvas.cpp touchhandler.cpp main.cpp \
+    $(pkg-config --cflags --libs sdl2) \
+    -lSDL2_image -lSDL2_ttf -levdev -lpthread
